5df530abbbd1d8a4930d0f91727ed26b64d73eb4
Muhammad Rehan Saeed <musa@microsoft.com>
2020-01-06 09:38:48 +0000

    Add IL code generation example

--- Source/TheNewKeyword/Program.cs
+++ Source/TheNewKeyword/Program.cs
@@ -2,33 +2,40 @@ namespace TheNewKeyword
 {
     using System;
     using System.Linq.Expressions;
+    using System.Reflection.Emit;
     using System.Runtime.Serialization;
 
     public class Program
     {
         public static void Main()
         {
+            // Create an instance using the new keyword (fast).
             var program1 = new Program();
 
             // Create an instance without calling the constructor.
             var program2 = FormatterServices.GetUninitializedObject(typeof(Program));
 
-            var program3 = Activator.CreateInstance<Program>();
+            // Create an instance using reflection (slow).
+            var program3 = typeof(Program).GetConstructor(Array.Empty<Type>()).Invoke(null);
 
-            var program4 = Factory<Program>.CreateInstance();
+            // Create an instance using dynamically (slow).
+            var program4 = Activator.CreateInstance<Program>();
 
-            // Writing low level IL code to new up an instance.
+            // Create an instance using expression trees (medium speed).
+            var createProgram1 = Expression.Lambda<Func<Program>>(Expression.New(typeof(Program))).Compile();
+            var program5 = createProgram1();
 
-            // Using T4 templates or new templating engine to generate design time code which uses the new keyword.
-        }
+            // Writing low level IL code to new up an instance (medium speed).
+            var constructor = typeof(Program).GetConstructor(Array.Empty<Type>());
+            var dynamicMethod = new DynamicMethod("CreateProgram", typeof(Program), parameterTypes: null);
+            var il = dynamicMethod.GetILGenerator();
+            il.Emit(OpCodes.Newobj, constructor);
+            il.Emit(OpCodes.Ret);
+            var createProgram2 = (Func<Program>)dynamicMethod.CreateDelegate(typeof(Func<Program>));
+            var program6 = createProgram2();
 
-        public static class Factory<T>
-            where T : new()
-        {
-            private static readonly Func<T> CreateInstanceFunc =
-                Expression.Lambda<Func<T>>(Expression.New(typeof(T))).Compile();
-
-            public static T CreateInstance() => CreateInstanceFunc();
+            // Using T4 templates or new templating engine to generate design time code which uses the new keyword (fast).
+            // Not shown
         }
     }
 }
